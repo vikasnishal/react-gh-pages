e5639f8aebe3dc9465e0794bf8f36d50912976cc
vikas kumar <vikas@simplotel.com>
2018-11-15 15:10:20 +0530

    Updates

---
+++ precache-manifest.f9828ab85ab8df722eca91e79e5a6978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "246139ec5384d1df2276",
+    "url": "/react-gh-pages/static/js/main.246139ec.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-gh-pages/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "246139ec5384d1df2276",
+    "url": "/react-gh-pages/static/css/main.0cc40c94.chunk.css"
+  },
+  {
+    "revision": "ad0dec7a9788007c89770d510bf53f2a",
+    "url": "/react-gh-pages/index.html"
+  }
+];
